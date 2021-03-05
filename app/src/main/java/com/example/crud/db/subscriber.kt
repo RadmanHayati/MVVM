00f4f1d93394cd53f21072fc2e3cc48cf24b1fdd
Radman Hayati <radman.hayatii@gmail.com>
2021-03-05 17:41:33 +0330

    subscriber dataModel is ready by radman hayati

---
+++ app/src/main/java/com/example/crud/db/subscriber.kt
@@ -0,0 +1,16 @@
+package com.example.crud.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "subscriber_data_table")
+data class subscriber(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "subscriber_id")
+    val id:Int,
+    @ColumnInfo(name="subscribe_name")
+    val name:String,
+    @ColumnInfo(name="subscriber_email")
+    val email:String
+)
